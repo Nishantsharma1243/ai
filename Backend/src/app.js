39d0d29bd0d364079d12fda807e0dce050a69f2c
Nishant Sharma <135991298+Nishantsharma1243@users.noreply.github.com>
2025-02-15 10:01:59 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,17 @@
 const express = require('express')
+require('dotenv').config();
 const app = express();
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 
 app.use(cors())
+const corsConfig ={
+    origin:process.env.Client_URL,
+    credentials:true,
+    method:["GET","POST","DELETE"],
+}
+app.options("",cors(corsConfig));
+app.use(cors(corsConfig));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
